080324a21672fef2ba2103093a98a5d56383e5ba
lloydi <github@lloydi.com>
2021-07-04 21:42:16 +0100

    Added feature to detect/highlight elements with duplicated accnames
    commit a2ca058a8a9377ea10b91f20c27fb6e30467f429
    Author: lloydi <github@lloydi.com>
    Date:   Sun Jul 4 15:25:22 2021 +0100
    
        Highlight first instance of duplicated accName
    
    commit 7ccbf6f4265062af88df23ae208871fbc10bf61b
    Author: lloydi <github@lloydi.com>
    Date:   Sat Jul 3 13:44:46 2021 +0100
    
        Added a test page for checking duplicate accNames
    
    commit 80a1a3d96669a8ea0e2fd1852766007bfe538b1b
    Author: lloydi <github@lloydi.com>
    Date:   Wed Jun 23 10:04:49 2021 +0100
    
        Update wtfocus.js
    
    commit 2e32d1e4ed5b868e67bb36099b3f5f8fc19269ff
    Author: lloydi <github@lloydi.com>
    Date:   Wed Jun 23 10:01:51 2021 +0100
    
        Stopped no acc names showing as duplicates
    
    commit 577345333088a470e1c155f36ad09e7a41a4cc2b
    Author: lloydi <github@lloydi.com>
    Date:   Wed Jun 23 09:37:02 2021 +0100
    
        removed warning alert symbol from HTML element
    
    commit 72cd5ff1c3abc220282b3b9ee142749526cd9226
    Author: lloydi <github@lloydi.com>
    Date:   Wed Jun 23 08:35:07 2021 +0100
    
        Tweaked messaging on dupes
    
    commit fc4f651e820839cc820e6f95a425d69d4dacd737
    Author: lloydi <github@lloydi.com>
    Date:   Wed Jun 23 08:13:10 2021 +0100
    
        Added warning color for duplicated accName
    
    commit cf1165eb33c2afd46c4a7c8fd72a21eb22081ef7
    Author: lloydi <github@lloydi.com>
    Date:   Wed Jun 23 00:32:55 2021 +0100
    
        Dupe detection - first pass

--- wtfocus.js
+++ wtfocus.js
@@ -1,7 +1,7 @@
 "use strict";
 // TODO
-// change to <dl>
-// add option to output to page or console (currently seet to both)
+// change to <dl>?
+// add option to output to page or console (currently seet to both)?
 
 function WTFocus() {
   const focusables = document.querySelectorAll('a[href],button,select,input:not([type="hidden"]),textarea,summary,details,area,[tabindex]:not(#WTFocusPanel):not([tabindex="-1"]),[contenteditable]:not([contenteditable="false"])');
@@ -25,6 +25,9 @@ function WTFocus() {
 
   let isGood = false;
   let isBad = false;
+  let isDupeAccName = false;
+  let dupeAccNameIsNoAccName = false;
+  let currentActiveEl;
 
   function findAncestor(el, sel) {
     while ((el = el.parentElement) && !(el.matches || el.matchesSelector).call(el, sel));
@@ -65,7 +68,7 @@ function WTFocus() {
     const focusStyles = document.createElement("style");
     focusStyles.setAttribute("type", "text/css");
     focusStyles.setAttribute("id", "panelStyles");
-    focusStyles.textContent = ".WTFocusTempFocusStyle:focus {outline:" + WTFfocusOutlineWidth + "px solid black!important;outline-offset:" + WTFfocusOutlineWidth + "px!important;overflow:visible;/*background:yellow!important;color:black!important;*/} .visually-hidden {clip-path: inset(100%);clip: rect(1px, 1px, 1px, 1px);height: 1px;overflow: hidden;position: absolute;white-space: nowrap;width: 1px;}";
+    focusStyles.textContent = ".dupeAccName {outline:4px dashed #CC3300!important;outline-offset:" + WTFfocusOutlineWidth + "px!important;overflow:visible;} .WTFocusTempFocusStyle:focus {outline:" + WTFfocusOutlineWidth + "px solid black!important;outline-offset:" + WTFfocusOutlineWidth + "px!important;overflow:visible;/*background:yellow!important;color:black!important;*/} .WTFocusTempFocusStyle.dupeAccName:focus {outline-color:#CC3300!important;} .visually-hidden {clip-path: inset(100%);clip: rect(1px, 1px, 1px, 1px);height: 1px;overflow: hidden;position: absolute;white-space: nowrap;width: 1px;}";
     document.querySelector("body").appendChild(focusStyles);
   }
   function addPanelStyles(WTFpanelWidth) {
@@ -73,9 +76,9 @@ function WTFocus() {
     consoleStyle.setAttribute("type", "text/css");
     consoleStyle.setAttribute("id", "focusStyles");
     consoleStyle.textContent =
-      "#WTFocusPanel.error {background:darkred;} #WTFocusPanel[hidden] {display:none;} #WTFocusPanel {border:2px solid #fff;z-index:1000;text-shadow:none;font-family:sans-serif;display:block;text-align:left;position: absolute;z-index:10000;background: black;padding: 20px 20px;width:" +
+      "#WTFocusPanel.error {background:darkred;} #WTFocusPanel.warning {background:#CC3300;} #WTFocusPanel[hidden] {display:none;} #WTFocusPanel {border:2px solid #fff;z-index:1000;text-shadow:none;font-family:sans-serif;display:block;text-align:left;position: absolute;z-index:10000;background: black;padding: 20px 20px;width:" +
       WTFpanelWidth +
-      "px;font-size:16px;} #WTFocusPanel button {font-weight:bold;background:none;color:#fff;padding:3px 10px;font-size:14px;border:1px solid #fff;display:inline-block;margin:10px 0 -10px 0;} #WTFocusPanel ul,#WTFocusPanel li {margin:0;padding:0;list-style:none} #WTFocusPanel li {margin:3px 0;background:#fff;color:#333;padding:2px} #WTFocusPanel li.outline {outline:4px solid rgb(58, 190, 58);outline-offset:-4px;padding:8px} #WTFocusPanel.error:before {background:darkred} #WTFocusPanel:before {content:'';display:block;height:20px;width:20px;transform:rotate(45deg);position:absolute;background:#000;left:-12px;top:3px;border:2px solid #fff;border-right:none;border-top:none;} #WTFocusPanel.toBottom:before {top:auto;bottom:3px} #WTFocusPanel.toLeft:before {left:auto;right:-12px;border:2px solid #fff;border-left:none;border-bottom:none;} /*#WTFocusPanel:after {content:'WTFocus';position:absolute;bottom:20px;right:20px;color:silver;font-size:14px;}*/";
+      "px;font-size:16px;} #WTFocusPanel button {font-weight:bold;background:none;color:#fff;padding:3px 10px;font-size:14px;border:1px solid #fff;display:inline-block;margin:10px 0 -10px 0;} #WTFocusPanel ul,#WTFocusPanel li {margin:0;padding:0;list-style:none} #WTFocusPanel li {margin:3px 0;background:#fff;color:#333;padding:2px} #WTFocusPanel li.outline {outline:4px solid rgb(58, 190, 58);outline-offset:-4px;padding:8px} #WTFocusPanel.error:before {background:darkred} #WTFocusPanel.warning:before {background:#CC3300} #WTFocusPanel:before {content:'';display:block;height:20px;width:20px;transform:rotate(45deg);position:absolute;background:#000;left:-12px;top:3px;border:2px solid #fff;border-right:none;border-top:none;} #WTFocusPanel.toBottom:before {top:auto;bottom:3px} #WTFocusPanel.toLeft:before {left:auto;right:-12px;border:2px solid #fff;border-left:none;border-bottom:none;} /*#WTFocusPanel:after {content:'WTFocus';position:absolute;bottom:20px;right:20px;color:silver;font-size:14px;}*/";
     document.querySelector("head").appendChild(consoleStyle);
   }
   function promptForLoggingType() {
@@ -107,6 +110,9 @@ function WTFocus() {
       removePanel();
     });
     WTFocusPanel.appendChild(consoleClose);
+  }
+  function hidePanel() {
+    document.querySelector("#WTFocusPanel").setAttribute("hidden","hidden");
   }
   function removePanel() {
     document.querySelector("#WTFocusPanel").remove();
@@ -164,6 +170,7 @@ function WTFocus() {
   promptForLoggingType();
   addCloseButton();
 
+  let accNamesFound = [];
   Array.from(focusables).forEach(function (focusable) {
     focusable.classList.add("WTFocusTempFocusStyle");
 
@@ -172,7 +179,9 @@ function WTFocus() {
       styleEl.remove();
     });
 
+
     focusable.addEventListener("focus", () => {
+      removeDupeIndicators();
       let containsImages = false;
       isGood = false;
       isBad = false;
@@ -360,20 +369,52 @@ function WTFocus() {
       console.log("%cACTIVE ELEMENT: ", style_title_formatting);
       console.log(focusable);
       // }
-      if (accName === "") {
-        if (outputToPage) {
-          WTFocusPanel.classList.add("error");
+
+      isDupeAccName = (focusable.getAttribute("data-dupe")==="true");
+      dupeAccNameIsNoAccName = (isDupeAccName && (accName === ""));
+
+      if ((accName === "")||isDupeAccName) {
+        if (accName === "") {
+          isBad = true;
+          if (outputToPage) {
+            WTFocusPanel.classList.add("error");
+          }
+          // if (!isDupeAccName) {
+            log("Accessible Name: No accessible name!", "", style_bad_formatting);
+            log("Accessible Name Source: N/A", "", style_bad_formatting);
+          // }
         }
-        isBad = true;
-        log("Accessible Name: No accessible name!", "", style_bad_formatting);
-        log("Accessible Name Source: N/A", "", style_bad_formatting);
+        if (isDupeAccName && (accName !== "")) {
+          if (outputToPage) {
+            WTFocusPanel.classList.add("warning");
+          }
+          //reveal other dupes
+          const allDupeAccNames = document.querySelectorAll("[data-accname='" + accName + "']");
+          const dupeCount = allDupeAccNames.length;
+          log("Accessible Name: ", accName, style_bad_formatting);
+          if (!dupeAccNameIsNoAccName) {
+            Array.from(allDupeAccNames).forEach(function (anotherDupe) {
+              anotherDupe.classList.add("dupeAccName");
+            });
+            log("Duplicate warning!", dupeCount + " elements on page have the same accessible name", style_bad_formatting);
+          }
+          console.log("Elements on page that have identical accessible names:");
+          Array.from(allDupeAccNames).forEach(function (anotherDupe) {
+            console.log(anotherDupe);
+          });
+          log("Accessible Name Source: ", accNameSource, style_bad_formatting);
+
+        }
       } else {
         if (outputToPage) {
           WTFocusPanel.classList.remove("error");
+          WTFocusPanel.classList.remove("warning");
         }
         log("Accessible Name: ", accName, style_good_formatting);
         log("Accessible Name Source: ", accNameSource, style_good_formatting);
       }
+
+      isBad=false;
       log("HTML Element: ", tagDetails, style_good_formatting);
       if (!outputToPage) {
         console.log("%cACCESSIBLE NAME COMES FROM: ", style_title_formatting);
@@ -485,7 +526,60 @@ function WTFocus() {
       Array.from(allTempNodes).forEach(function (tempNode) {
         tempNode.remove();
       });
+
+      //add accNAme as attribute for easier lookup/comparison
+      focusable.setAttribute("data-accname",accName);
+
+
+      if (!checkedForDupes) {
+        addAccNameIfNotAlreadyExists(accName,focusable);
+      }
     });
   });
+
+  let checkedForDupes=false;
+  function removeDupeIndicators() {
+    Array.from(focusables).forEach(function (focusable) {
+      focusable.classList.remove("dupeAccName");
+    });
+  }
+
+  function mimicFocus(){
+    currentActiveEl = document.activeElement;
+    Array.from(focusables).forEach(function (focusable) {
+      if (document.activeElement===focusable) {
+        focusable.blur();
+      }
+      focusable.focus();
+    });
+    checkedForDupes=true;
+    if (currentActiveEl.tagName==="BODY") {
+      //focus on body
+      const bodyEl = document.querySelector("body");
+      bodyEl.setAttribute("tabindex","-1");
+      bodyEl.focus();
+      hidePanel();
+    } else {
+      currentActiveEl.focus();
+    }
+    console.clear();
+  }
+  mimicFocus();
+
+  function addAccNameIfNotAlreadyExists(thisAccName,el) {
+    let dupeFound=false;
+    Array.from(accNamesFound).forEach(function (accName) {
+      if (accName===thisAccName) {
+        dupeFound=true;
+      }
+    });
+    if (dupeFound) {
+      el.setAttribute("data-dupe","true");
+      const previousElWithSameAccName = document.querySelector("[data-accname='" + thisAccName + "']");
+      previousElWithSameAccName.setAttribute("data-dupe","true");
+    } else {
+      accNamesFound.push(thisAccName);
+    }
+  }
 }
 WTFocus();
